2d8401e5ee327bcdc7620969b398bd770484e59d
hxdls1 <hdelossantos@udallas.edu>
2016-04-12 23:13:00 -0500

    Homework 9
    Added or changed the following methods to DSArrayList:
    add()
    remove()
    insert()
    pop()
    getSize()
    get()
    removeGaps()
    
    Added the following fields to DSArrayList:
    private int size
    private final int INITIAL_SIZE
    
    INITIAL_SIZE is how big to make the array when a DSArrayList is
    constructed.
    
    Changed Tester to test the updated DSArrayList code.

--- src/ds2016/DSArrayList.java
+++ src/ds2016/DSArrayList.java
@@ -16,26 +16,35 @@ public class DSArrayList< J > {
 	 * The backing array. Stores the (references to) objects
 	 */
 	private J[] jays;
-
+	
+	private int size = 0;
+	
+	private final int INITIAL_SIZE = 10;
 
 	/**
 	 * Constructor
 	 */
 	public DSArrayList(){
-		jays = (J[])(new Object[10]);
+		jays = (J[])(new Object[INITIAL_SIZE]);
 	}
 
 	/**
-	 * Simply insert something at the end of the DSArrayList
+	 * "Simply" insert something at the end of the DSArrayList
 	 */
 	public void add(J thingToAdd){
-		for (int i = 0; i < this.jays.length; i++){
-			if (this.jays[i] == null) {
-				this.jays[i] = thingToAdd;
-				break;
+		// If the size is the same as jays.length or greater, we need to resize the array
+		if(this.size >= this.jays.length)
+		{
+			J[] tempArray = (J[])(new Object[this.jays.length * 2]);
+			for(int i = 0; i < this.jays.length; i++)
+			{
+				tempArray[i] = this.jays[i];
 			}
+			
+			this.jays = tempArray;
 		}
-
+		
+		this.jays[this.size++] = thingToAdd;
 	}
 
 	/**
@@ -44,7 +53,10 @@ public class DSArrayList< J > {
 	 * @param idx The index of the item to return
 	 */
 	public J get(int idx){
-		return this.jays[idx];
+		if(idx <= this.size && idx >= 0)
+			return this.jays[idx];
+		else 
+			return null;
 	}
 
 	/**
@@ -52,7 +64,12 @@ public class DSArrayList< J > {
 	 * 
 	 * @param idx The index of the item to change
 	 */
-	public void set(int idx, J newValue){}
+	public void set(int idx, J newValue){
+		if(idx >= this.size || idx < 0)
+			return;
+		
+		this.jays[idx] = newValue;
+	}
 
 	/**
 	 * Remove an item from the DSArrayList and close the gap
@@ -60,28 +77,93 @@ public class DSArrayList< J > {
 	 * 
 	 */
 	public void remove(int idx){
-		for (int i = idx; i < this.jays.length - 1; i++){
-			this.jays[i] = this.jays[1 + i];
+		// If the given index is greater than the size of the array, exit
+		// Alternatively, we could throw an error
+		if(idx >= this.size) return;
+		
+		// set the indicated spot to null
+		this.jays[idx] = null;
+		
+		// If the index is less than size - 1, we need to rearrange the array
+		if(idx < size - 1)
+		{
+			this.removeGaps();
 		}
-		this.jays [this.jays.length - 1] = null;
+		else // otherwise just decrement the size, since we only removed the last entry
+		{
+			this.size--;
+		}
+	}
+	
+	public void removeGaps(){
+		J[] tempArray = (J[])(new Object[this.jays.length]);
+		
+		// Keep track of how big the tempArray gets
+		int p = 0;
+		
+		// Note: we loop to global size, not jays.length
+		// This is because we know that everything after jays[size] will be null
+		// And thus does not need to be copied
+		
+		for(int i = 0; i < this.size; i++)
+		{
+			if(jays[i] != null)
+				tempArray[p++] = jays[i];
+		}
+		
+		this.jays = tempArray;
+		
+		// Note: this method will filter out ALL null entries in the array
+		// Therefore, we set size to p rather than using size-- to ensure
+		// that the size is the same as the number of items in the new array
+		this.size = p;
 	}
 
 	/**
 	 * Returns the last item in the DSArrayList and removes it
 	 */
 	public J pop(){
-		J rval = null;
-		for (int i = 0; i < this.jays.length - 1; i++){
-			if (this.jays[i + 1] == null) {
-				rval = this.jays[i];
-				this.jays[i] = null;
-				break;
-				}
-		}
-		if (rval == null) {
-			rval = this.jays [this.jays.length - 1];
-			this.jays [this.jays.length - 1] = null;
-		}
+		J rval = this.jays[this.size - 1];
+
+		// call the remove method to avoid re-writing the same functionality
+		this.remove(this.size - 1);
+		
 		return rval;
+	}
+	
+	public int getSize(){
+		return this.size;
+	}
+	
+	/**
+	 * Inserts an item at the given position and readjusts the array
+	 * @param idx
+	 * @param thingToInsert
+	 */
+	public void insert(int idx, J thingToInsert)
+	{
+		if(idx == size)
+			// we already have a function for inserting something at the end of the list
+			this.add(thingToInsert);
+		else if(idx > size || idx < 0) // exit if index out of bounds
+			return;
+		
+		int p = 0;
+		
+		J[] tempArray = (J[])(new Object[this.jays.length + 1]);
+		
+		for(int i = 0; i < this.size + 1; i++)
+		{
+			if(i == idx)
+			{
+				tempArray[p++] = thingToInsert;
+				tempArray[p++] = this.jays[i];
+			}
+			else
+				tempArray[p++] = this.jays[i];
+		}
+		
+		this.jays = tempArray;
+		this.size = p - 1;
 	}
 }
--- src/ds2016/Tester.java
+++ src/ds2016/Tester.java
@@ -4,32 +4,37 @@
 
 package ds2016;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 class Tester{
 
-	public static void main(String args[]) throws IOException{
-		//Nim t = new Nim();
-		//TicTacToeGUI2 t = new TicTacToeGUI2();
-		TicTacToe t = new TicTacToe();
-		t.playGame();
-
-		FileReader f = new FileReader("/usr/share/dict/words");
-		BufferedReader reader = new BufferedReader(f);
-		String line;
+	public static void main(String args[]){
 		
-		DSArrayList <String> wordList = new DSArrayList<String>();
-		while ((line = reader.readLine()) != null) {
-			line = line.replaceAll("[\\?,~<01234567890()':;.!_-]", "");	
-			
-			wordList.add(line);
-			
-			
+		DSArrayList<String> someArray = new DSArrayList<String>();
+		
+		someArray.add("steel");
+		someArray.add("cheese");
+		someArray.add("useless");
+		
+		System.out.println(someArray.get(0));
+		
+		for(int i = 0; i < someArray.getSize(); i++)
+		{
+			System.out.println(someArray.get(i));
 		}
-		System.out.println("wordList has " + wordList.getSize() + " items");
-
+		
+		System.out.println(someArray.getSize());
+		
+		System.out.printf("Pop! We got: %s%n", someArray.pop());
+		
+		System.out.println(someArray.getSize());
+		
+		someArray.add("neptune");
+		someArray.add("noire");
+		
+		someArray.insert(2,  "processor unit");
+		
+		for(int i = 0; i < someArray.getSize(); i++)
+		{
+			System.out.println(someArray.get(i));
+		}
 	}
 }
