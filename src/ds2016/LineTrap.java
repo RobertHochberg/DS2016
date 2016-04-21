2edadca529685a57c648b8cb43b4d8fd5968ee3e
plieblang <plieblang@users.noreply.github.com>
2016-04-21 17:57:49 -0500

    Reduces LineTrap to constructor and drawBoard(); alters Tester
    Comments out all code in LineTrap except for constructor and drawBoard()
    so as to test those two
    Tester now only starts LineTrap

--- src/ds2016/LineTrap.java
+++ src/ds2016/LineTrap.java
@@ -1,6 +1,7 @@
 /**
  * draw lines until you can't.
  * rowLocation++ moves up/down; colLocation moves left/right
+ * consider having non-square grid; would require another field
  */
 
 package src.ds2016;
@@ -9,12 +10,13 @@ import java.util.Scanner;
 
 class LineTrap extends AlternatingGame{
 	int numGridPoints;
-	char[][] board = new char[numGridPoints][numGridPoints]; //row 1st, col 2nd
+	char[][] board;; //row 1st, col 2nd
 	int rowLocation;
 	int colLocation;
 	int whoseTurn = 1;
 	char VERTICALLINE = '\u2502';
 	char HORIZONTALLINE = '\u2500';
+	char DOT = '*';
 	char MARKER = '@';
 	char EMPTYSPACE = ' ';
 	// int numMoves = 0; // if time, include for stats
@@ -24,10 +26,12 @@ class LineTrap extends AlternatingGame{
 	public LineTrap(){
 		//sets grid size through user input
 		scanner = new Scanner(System.in);
-		System.out.print("Please enter game size (must be odd):  ");
+		System.out.print("Please enter game size (must be odd): ");
 		do {
 			numGridPoints = scanner.nextInt();
-		} while (numGridPoints > 3 && numGridPoints % 2 != 0); //insert an upper limit as appropriate
+		} while (numGridPoints < 3 || numGridPoints % 2 != 1); //insert an upper limit as appropriate
+
+		board = new char[numGridPoints][numGridPoints];
 
 		rowLocation = (int)Math.ceil((double)numGridPoints/2);
 		colLocation = (int)Math.ceil((double)numGridPoints/2);
@@ -36,7 +40,7 @@ class LineTrap extends AlternatingGame{
 		for(int row = 0; row < numGridPoints; row++){
 			for(int col = 0; col < numGridPoints; col++){
 				if (row%2 == 0 && col%2 == 0) {
-					board[row][col] = '*';
+					board[row][col] = DOT;
 				}
 			}
 		}
@@ -48,13 +52,14 @@ class LineTrap extends AlternatingGame{
 	public void drawBoard(){
 		for(int row = 0; row < numGridPoints; row++){
 			for(int col = 0; col < numGridPoints; col++){
-				System.out.println(board[row][col]);
+				System.out.print(board[row][col]);
 			}
 		}
 	}
 
+	/**
 	@Override
-	void setBoard(Object nb) {
+	void setBoard(Object nb){
 		char[][]newBoard = (char[][])nb;
 		for(int row = 0; row < numGridPoints; row++){
 			for(int col = 0; col < numGridPoints; col++){
@@ -64,21 +69,23 @@ class LineTrap extends AlternatingGame{
 	}
 
 	@Override
-	Object getBoard() {
+	Object getBoard(){
 		return board;
 	}
 
 	@Override
 	// consider not printing "Please make a move" or equivalent out
 	// need to protect against moves to outside the board
-	void getHumanMove() {
+	void getHumanMove(){
 		int newUpLocation = rowLocation + 2;
 		int newDownLocation = rowLocation - 2;
 		int newRightLocation = colLocation - 2;
 		int newLeftLocation = colLocation + 2;
+		String humanMove;
+
 		do {
 			System.out.println("Please make a move");
-			String humanMove = scanner.next();
+			humanMove = scanner.next();
 			if (humanMove.indexOf("w") != -1 && rowLocation != 0 && rowLocation != numGridPoints) {
 				board[rowLocation++][colLocation] = VERTICALLINE;
 				board[newUpLocation][colLocation] = MARKER;
@@ -96,43 +103,42 @@ class LineTrap extends AlternatingGame{
 				board[rowLocation][newLeftLocation] = MARKER;
 				colLocation = newLeftLocation;
 			}
-		} while (humanMove.length() != 1 && humanMove.indexOf("w", "s", "a", "d") == -1);
+		} while (humanMove.length() != 1 && humanMove.indexOf("w") == -1 && humanMove.indexOf("s") == -1 && humanMove.indexOf("a") == -1 && humanMove.indexOf("d") == -1);
 
 		whoseTurn = 3-whoseTurn;
 		// numMoves++;
 	}
 
 	@Override
-	// might need to change depending on tree size, which is related to numGridPoints
-	// or is tree size controlled through getNumChildren()
-	void getComputerMove() {
-		return getSmartComputerMove;
+	void getComputerMove(){
+		getSmartComputerMove();
 		// numMoves++;
 	}
 
 	@Override
-	int whoseTurn(Object localBoard) {
+	int whoseTurn(Object localBoard){
 		return whoseTurn(localBoard);
 	}
 
 	@Override
-	boolean isGameOver() {
+	boolean isGameOver(){
 		if (rowLocation++ == VERTICALLINE && rowLocation-- == VERTICALLINE && colLocation++ == HORIZONTALLINE && colLocation-- == HORIZONTALLINE) {
 			return true;
 		} else return false;
 	}
 
 	@Override
-	int whoWon() {
+	int whoWon(){
 		return whoseTurn;
 	}
 
 	@Override
-	int whoWon(Object board) {
+	int whoWon(Object board){
 		if (whoseTurn == 1) {
 			return 2;
 		} else return 1;
 	}
+	 **/
 
 	/**
 	 * logic for smart game play implemented here
@@ -142,9 +148,9 @@ class LineTrap extends AlternatingGame{
 	 * pick optimal move by propagating winners up the tree and then choosing
 	 * 
 	 * place lines and then switch whose turn
-	 */
+	 *
 	@Override
-	Object getChildren(Object board) {
+	Object[] getChildren(Object board){
 		char[][] parentBoard = (char[][])board;
 		char[][] child; //this probably shouldn't exist at such a large scope
 		int childWhoseTurn = 3 - whoseTurn; //advantages vs. whoseTurn() method
@@ -160,12 +166,13 @@ class LineTrap extends AlternatingGame{
 				} else 
 			}
 		}
-		for(int i = rowLocation; i > 0; i--){
-			for(int j = colLocation; j > 0; j--){
+		for(int k = rowLocation; k > 0; k--){
+			for(int l = colLocation; l > 0; l--){
 				if (!isGameOver()) {
 					child = parentBoard;
 				} else 
 			}
 		}
 	}
+	 **/
 }
--- src/ds2016/Tester.java
+++ src/ds2016/Tester.java
@@ -5,23 +5,24 @@
 
 package src.ds2016;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+//import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
 
 class Tester{
 
-	public static void main(String args[]) throws IOException{
+	public static void main(String args[]){
 
-		arrayTest();
+		//arrayTest();
 
-		Nim t = new Nim();
-		System.out.println("# game plays is " + t.buildTree(t.getBoard()).numLeaves());
+		LineTrap t = new LineTrap();
+		//System.out.println("# game plays is " + t.buildTree(t.getBoard()).numLeaves());
 		//TicTacToeGUI2 t = new TicTacToeGUI2();
 		//TicTacToe t = new TicTacToe();
 		t.playGame();
 
+		/**
 		FileReader f = new FileReader("/usr/share/dict/words");
 		BufferedReader reader = new BufferedReader(f);
 		String line;
@@ -38,22 +39,22 @@ class Tester{
 
 	}
 
-	
+
 	public static void arrayTest(){
 		DSArrayList<Integer> a = new DSArrayList<>();
-		
+
 		for(int i = 0; i < 10; i++)
 			a.add((3*i) % 10);
-			
+
 		a.insert(a.pop(), 6);
 		a.remove(2);
 		a.insert(a.pop(), 2);
 		a.remove(3);
-		
+
 		System.out.println("0 3 4 2 5 7 8 1 <=== Should be contents of line below ");
 		for(int i = 0; i < a.getSize(); i++)
 			System.out.print(a.get(i) + " ");
 		System.out.println("");
-		
+		 **/
 	}
 }
